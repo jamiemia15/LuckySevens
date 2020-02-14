ac79ec03689a95b6c237e36f42c696b0c112c31c
jamiemia15 <60367103+jamiemia15@users.noreply.github.com>
2020-02-14 00:08:59 -0500

    Add files via upload

--- lucky.js
+++ lucky.js
@@ -11,22 +11,24 @@ function validateItems() {
 }
 
 function play(startingBet) {
-    var money = startingBet;
+    var money = parseInt(startingBet);
     var diceOne = 0;
     var diceTwo = 0;
     var total = 0;
     var count = 0;
-    var max = 0;
+    var max = parseInt(startingBet);
     var countAtMax = 0;
     while (money > 0) {
         count++;
-        diceOne = Math.floor(Math.random * 6) + 1;
-        diceTwo = Math.floor(Math.random * 6) + 1;
-        total = diceOne + diceTwo; 
+        diceOne = Math.floor(Math.random() * 6) + 1;
+        diceTwo = Math.floor(Math.random() * 6) + 1;
+        total = diceOne + diceTwo;
         if (total == 7) {
             money = money + 4;
-            max = max + 4
-            countAtMax = count;
+            if (money > max) {
+                max = money;
+                countAtMax = count;
+            }
         } 
         else {
             money = money - 1;
